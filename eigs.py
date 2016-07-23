488ac0c069756b2a7433332902d225988bdc9f6e
ciubecca <lorenzovitale89@gmail.com>
2016-07-23 18:55:17 +0200

    updated eigs.py

--- eigs.py
+++ eigs.py
@@ -1,80 +1,69 @@
 import finiteVolH
-import phi1234
+import phi4
 import renorm
 import sys
 import scipy
 import math
-from scipy import optimize
-import json
 import database
 
+json = False
+occmax = 4
+m = 1.
+sigma = -30.
+neigs = 1
+klist = (1,-1)
 
 def main(argv):
     if len(argv) < 3:
-        print(argv[0], " <Emax> <g> <fname>")
+        print(argv[0], " <L> <Emax> <g>")
         return -1
 
-    Emax = float(argv[1])
-    g = float(argv[2])
-    fname = argv[3]
+    L = float(argv[1])
+    Emax = float(argv[2])
+    g = float(argv[3])
 
-    # Hardcoded parameters
-    m=1.
-    sigma = -30.
-    neigs = 5
+    if json == False:
+        db = database.Database()
+    else:
+        db = database.Database(dbname="data/spectraJson.db", useJson=True)
 
-    a = phi1234.Phi1234()
+    a = phi4.Phi4()
 
-    a.loadMatrix(fname)
-    L = a.L
+    for k in klist:
+        print("k=", k)
+        a.buildBasis(Emax=Emax, L=L, m=m, k=k, occmax=occmax)
+        print("Basis size: ", a.basis[k].size)
 
-    a.buildBasis(k=1, Emax=Emax)
-    a.buildBasis(k=-1, Emax=Emax)
+        a.buildMatrix(k=k)
+        a.setCouplings(0, 0, g)
+        # b = finiteVolH.FiniteVolH(a.L, m)
+        # g0, g2, g4 = b.directCouplings(g)
 
-    print('k=1 basis size = ',  a.basis[1].size)
-    print('k=-1 basis size = ',  a.basis[-1].size)
+        # Emaxbar == Emax means there are no tails
+        approxQuery = {"g":g, "L":L, "Emaxbar":Emax, "Emax":Emax}
+        exactQuery = {"k":k}
+        if db.getObjList('spec', approxQuery=approxQuery, exactQuery=exactQuery) != []:
+            print("Eigenvalues already present")
+            continue
 
-    db = database.Database()
+        Er = 0
+        for ren in ("raw","renlocal"):
+            # This is different wrt with tails
+            a.renlocal(Emax=Emax, Er=Er)
+            a.computeHamiltonian(Emax=Emax, k=k, ren=ren, Er=Er, addTails=False)
 
-    approxQuery = {"g":g, "L":L, "Emax":Emax}
-    if db.getObjList('spec', approxQuery=approxQuery) != []:
-        print("Eigenvalues already present")
-        return
+            compsize = a.compH.shape[0]
+            print("Comp basis size: ", a.compH.shape[0])
 
-    b = finiteVolH.FiniteVolH(a.L, m)
-    g0, g2, g4 = b.directCouplings(g)
+            a.computeEigval(k=k, ren=ren, sigma=sigma, neigs=neigs)
+            Er = a.vacuumE(ren="raw")
 
-    a.setCouplings(g0=g0, g2=g2, g4=g4)
-    print("Computing raw eigenvalues for g0,g2,g4 = ", a.g0,a.g2,a.g4)
+            print("{} vacuum: ".format(ren), a.vacuumE(ren=ren))
 
-    a.computeHamiltonian(k=1, ren=False)
-    a.computeHamiltonian(k=-1, ren=False)
+            # If Emaxbar == Emax it means there are no tails
+            db.insert(k=k, Emax=Emax, Emaxbar=Emax, L=a.L, ren=ren, g=g, spec=a.eigenvalues[ren][k],
+                    eigv=a.eigenvectors[ren][k], occmax=occmax, basisSize=compsize, neigs=neigs)
 
-    a.computeEigval(k=1, sigma=sigma, n=neigs, ren=False)
-    a.computeEigval(k=-1, sigma=sigma, n=neigs, ren=False)
-
-    print("Raw vacuum: ", a.vacuumE(ren="raw"))
-
-    # a.renlocal(Er=a.vacuumE(ren="raw"))
-
-    # print "Computing renormalized eigenvalues for g0r,g2r,g4r = ", a.g0r,a.g2r,a.g4r
-
-    # a.computeHamiltonian(k=1, ren=True)
-    # a.computeHamiltonian(k=-1, ren=True)
-
-    # a.computeEigval(k=1, sigma=sigma, n=num, ren=True, corr=True, cutoff=cutoff)
-    # a.computeEigval(k=-1, sigma=sigma, n=num, ren=True, corr=True, cutoff=cutoff)
-
-    # print "Renlocal vacuum: ", a.vacuumE(ren="renlocal")
-    # print "Rensubl vacuum: ", a.vacuumE(ren="rensubl")
-
-    for k in (1,-1):
-        db.insert(k=k, Emax=Emax, L=a.L, ren="raw", g=g, spec=a.eigenvalues[k],
-                 eigv=a.eigenvectors[k], basisSize=a.basis[k].size, neigs=neigs)
-        # db.insert(k=k, Emax=Emax, L=a.L, ren="renlocal", g=g, spec=a.eigsrenlocal[k], basisSize=a.basis[k].size, \
-                        # neigs=neigs, Er=a.Er, cutoff=a.cutoff, m=m)
-        # db.insert(k=k, Emax=Emax, L=a.L, ren="rensubl", g=g, spec=a.eigsrensubl[k], basisSize=a.basis[k].size, \
-                        # neigs=neigs, Er=a.Er, cutoff=a.cutoff, m=m)
 
 if __name__ == "__main__":
     main(sys.argv)
