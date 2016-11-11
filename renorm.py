89e89d6f8ec78beeaa91044eaee24946509e4bbf
ciubecca <lorenzovitale89@gmail.com>
2016-11-10 20:28:07 -0500

    minor modifications

--- renorm.py
+++ renorm.py
@@ -50,7 +50,7 @@ class renVV3():
     def __init__(self, ETlist, m, eps):
         self.neval = 10000
 # XXX reset this to 10000
-        self.neval = 1000
+        # self.neval = 1000
 
         self.ETlist = ETlist
 
@@ -89,7 +89,7 @@ class renVV3():
 
 
 # XXX reset this to 10000
-        # self.neval = 10000
+        self.neval = 10000
         # Bilocal corrections
         self.V0V4 = self.computeIntegral(3, phi0phi4_1)
         self.V2V4 = self.computeIntegral(2, phi2phi4_1)
