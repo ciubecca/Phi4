236e723d7eef7c8a2ab7a9f6154d6dc0d1fc387f
ciubecca <lorenzovitale89@gmail.com>
2016-12-01 15:56:27 -0500

    plot vs ET

---
+++ plotvsET.py
@@ -0,0 +1,120 @@
+import sys
+import matplotlib.pyplot as plt
+import scipy
+import math
+from scipy import pi, log, log10, array, sqrt, stats
+from matplotlib import rc
+from cycler import cycler
+import database
+
+output = "png"
+# renlist = ("raw", "renloc", "rentails")
+renlist = ("rentails",)
+
+rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+rc('text', usetex=True)
+
+klist = (1,)
+
+neigs = 3
+
+
+# Ratio between EL and ET
+ratioELET = 1.5
+# Ratio between ELp and ET
+ratioELpET = 1.5
+# Ratio between ELpp and ELp
+ratioELppELp = 1.5
+
+maxntails = 200
+
+
+def fignum(k):
+    if k==1:
+        return 1
+    elif k==-1:
+        return 2
+
+def plotvsET(ETlist):
+
+    xlist = ETlist
+
+    db = database.Database()
+
+    exactQuery = {"ren":"rentails"}
+    approxQuery = {"g":g, "L":L}
+    boundQuery = {"ntails": (0,maxntails)}
+
+    spectrum = {k:[] for k in klist}
+
+    for k in klist:
+        exactQuery["k"] = k
+
+        for ET in ETlist:
+
+            EL = ratioELET*ET
+            ELp = ratioELpET*ET
+            ELpp = ratioELppELp*ELp
+
+            approxQuery["ET"] = ET
+            approxQuery["EL"] = EL
+            approxQuery["ELp"] = ELp
+            approxQuery["ELpp"] = ELpp
+
+
+            # NOTE We select the entry with the highest ntails below maxntails
+            spectrum[k].append(db.getObjList('spec', exactQuery, approxQuery,
+                boundQuery, orderBy="ntails")[0])
+
+
+    # SPECTRUM
+    for k in klist:
+        plt.figure(fignum(k))
+        sp = array(spectrum[k])
+        for i in range(neigs):
+            data = sp[:,i]
+            plt.plot(xlist, data)
+
+
+
+argv = sys.argv
+
+
+if len(argv) < 5:
+    print(argv[0], "<L> <g> <ETmin> <ETmax>")
+    sys.exit(-1)
+
+L = float(argv[1])
+g = float(argv[2])
+ETmin = float(argv[3])
+ETmax = float(argv[4])
+
+ETlist = scipy.linspace(ETmin, ETmax, (ETmax-ETmin)*2+1)
+print("ETlist:", ETlist)
+
+
+params = {'legend.fontsize': 8}
+plt.rcParams.update(params)
+
+plt.rc('axes', prop_cycle=(cycler('color', ['r', 'g', 'b', 'y']) +
+    cycler('linestyle', ['-', '--', ':', '-.'])))
+
+
+plotvsET(ETlist)
+
+for k in klist:
+
+    title = r"$g$={0:.1f}, $L$={1:.1f}, $k$={2}".format(g,L,k)
+    fname = "g={0:.1f}_L={1:.1f}.{2}".format(g,L,output)
+    loc = "lower right"
+
+    plt.figure(fignum(k), figsize=(4., 2.5), dpi=300, facecolor='w', edgecolor='w')
+    plt.title(title)
+    plt.xlabel(r"$E_{T}$")
+    plt.ylabel(r"$E_i$")
+# plt.legend(loc=loc)
+
+    if k==1:
+        plt.savefig("evenvsET_"+fname)
+    else:
+        plt.savefig("oddvsET_"+fname)
