f960e76153a25497f8064875731fda816c79b1bb
Lorenzo Vitale <lorenzovital89@gmail.com>
2017-02-09 16:25:28 -0500

    minor mod

--- eigsvsG.py
+++ eigsvsG.py
@@ -8,7 +8,6 @@ import math
 import database
 
 glist = scipy.linspace(0.5, 3, 6)
-glist = scipy.linspace(1, 1, 1)
 print("glist", glist)
 
 memdbg = False
@@ -18,7 +17,7 @@ if memdbg:
 test = False
 if test:
     warnings.warn("Monte Carlo is OFF")
-loc3 = False
+loc3 = True
 if not loc3:
     warnings.warn("Not including local correction to DH3")
 
@@ -31,8 +30,6 @@ if not saveondb:
 m = 1
 # Number of eigenvalues to compute per sector
 neigs = 1
-# List of parity quantum numbers
-klist = (1,)
 
 # Ratio between EL and ET
 ratioELET = 3
@@ -43,16 +40,16 @@ ratioELppELp = 1.5
 
 
 argv = sys.argv
-if len(argv) < 3:
-    print(argv[0], "<L> <ET>")
+if len(argv) < 4:
+    print(argv[0], "<k> <L> <ET>")
     sys.exit(-1)
 
-L = float(argv[1])
-ET = float(argv[2])
+k = int(argv[1])
+L = float(argv[2])
+ET = float(argv[3])
 
-print("klist", klist)
 
-print("L, ET", L, ET)
+print("k, L, ET", k, L, ET)
 
 EL = ratioELET*ET
 ELp = ratioELpET*ET
@@ -69,81 +66,78 @@ def main():
     a = phi4.Phi4(m, L)
     a.buildBasis(Emax=ET)
 
+    # Compute the potential matrices in the low-energy space below ET
+    a.computePotential(k)
 
-    for k in klist:
+    print("k=", k)
+    print("Full basis size: ", a.basis[k].size)
 
-        # Compute the potential matrices in the low-energy space below ET
-        a.computePotential(k)
-
-        print("k=", k)
-        print("Full basis size: ", a.basis[k].size)
-
-        a.setglist(glist=glist)
+    a.setglist(glist=glist)
 
 
 # Compute the raw eigenvalues for cutoff ET
-        a.computeEigval(k, ET, "raw", neigs=neigs)
-        epsraw = {g: a.eigenvalues[g]["raw"][k][0] for g in glist}
-        print("Raw vacuum:", epsraw)
+    a.computeEigval(k, ET, "raw", neigs=neigs)
+    epsraw = {g: a.eigenvalues[g]["raw"][k][0] for g in glist}
+    print("Raw vacuum:", epsraw)
 
-        # return
+    # return
 
-        # Always consider maximal set of tails
-        basisl = a.basis[k]
-        print("Total number of tails:", basisl.size)
+    # Always consider maximal set of tails
+    basisl = a.basis[k]
+    print("Total number of tails:", basisl.size)
 
-        if memdbg:
-            print("memory taken before computeLEVs", memory_usage())
+    if memdbg:
+        print("memory taken before computeLEVs", memory_usage())
 
-        a.computeLEVs(k, basisl, loc3=loc3)
+    a.computeLEVs(k, basisl, loc3=loc3)
 
-        if memdbg:
-            print("memory taken before genHEBasis", memory_usage())
+    if memdbg:
+        print("memory taken before genHEBasis", memory_usage())
 
-        print("Generating high energy basis...")
-        # Generate the high-energy "selected" basis by passing a set of tails
-        # and a maximum cutoff EL
-        a.genHEBasis(k, EL=EL, ELp=ELp, ELpp=ELpp)
-        print("Size of HE basis:", a.basisH[k].size)
+    print("Generating high energy basis...")
+    # Generate the high-energy "selected" basis by passing a set of tails
+    # and a maximum cutoff EL
+    a.genHEBasis(k, EL=EL, ELp=ELp, ELpp=ELpp)
+    print("Size of HE basis:", a.basisH[k].size)
 
 
-        if memdbg:
-            print("memory taken before computeHEVs", memory_usage())
+    if memdbg:
+        print("memory taken before computeHEVs", memory_usage())
 
-        print("Computing high energy matrices...")
+    print("Computing high energy matrices...")
 # Compute the matrices VLH, VHL, VHH, for the highest local cutoff ELmax.
 # Later we will be varying EL, therefore taking submatrices of these.
 # Computing VHH is expensive
-        a.computeHEVs(k)
+    a.computeHEVs(k)
 
-        a.computeEigval(k, ET, "renloc", eps=epsraw, neigs=neigs)
-        eps = {g: a.eigenvalues[g]["renloc"][k][0] for g in glist}
-        print("Local ren vacuum:", eps)
+    a.computeEigval(k, ET, "renloc", eps=epsraw, neigs=neigs)
+    eps = {g: a.eigenvalues[g]["renloc"][k][0] for g in glist}
+    print("Local ren vacuum:", eps)
 
-        if loc3:
-            a.calcVV3(ELp, eps, test=test)
+    if loc3:
+        a.calcVV3(ELp, eps, test=test)
 
-        a.computeEigval(k, ET, "rentails", EL=EL, ELp=ELp, ELpp=ELpp, eps=eps,
-                neigs=neigs, memdbg=memdbg, loc3=loc3)
-        print("Non-Local ren vacuum:", {g: a.eigenvalues[g]["rentails"][k][0]
-            for g in glist})
+    a.computeEigval(k, ET, "rentails", EL=EL, ELp=ELp, ELpp=ELpp, eps=eps,
+            neigs=neigs, memdbg=memdbg, loc3=loc3)
+    print("Non-Local ren vacuum:", {g: a.eigenvalues[g]["rentails"][k][0]
+        for g in glist})
 
-        print("Number of tails:", a.ntails)
+    print("Number of tails:", a.ntails)
 
-        if saveondb:
-            for g in glist:
-                datadict = dict(k=k, ET=ET, L=L, ren="raw", g=g, neigs=neigs,
-                        basisSize=a.compSize)
-                db.insert(datadict=datadict, spec=a.eigenvalues[g]["raw"][k])
+    if saveondb:
+        for g in glist:
+            datadict = dict(k=k, ET=ET, L=L, ren="raw", g=g, neigs=neigs,
+                    basisSize=a.compSize)
+            db.insert(datadict=datadict, spec=a.eigenvalues[g]["raw"][k])
 
-                datadict = dict(k=k, ET=ET, L=L, ren="renloc", g=g, eps=epsraw[g],
-                        neigs=neigs, basisSize=a.compSize)
-                db.insert(datadict=datadict, spec=a.eigenvalues[g]["renloc"][k])
+            datadict = dict(k=k, ET=ET, L=L, ren="renloc", g=g, eps=epsraw[g],
+                    neigs=neigs, basisSize=a.compSize)
+            db.insert(datadict=datadict, spec=a.eigenvalues[g]["renloc"][k])
 
-                datadict = dict(k=k, ET=ET, L=L, ren="rentails", g=g, EL=EL, ELp=ELp,
-                        ELpp=ELpp, ntails=a.ntails, eps=eps[g], neigs=neigs,
-                        basisSize=a.compSize, tailsComputedAtET=ET, maxntails=None)
-                db.insert(datadict=datadict, spec=a.eigenvalues[g]["rentails"][k])
+            datadict = dict(k=k, ET=ET, L=L, ren="rentails", g=g, EL=EL, ELp=ELp,
+                    ELpp=ELpp, ntails=a.ntails, eps=eps[g], neigs=neigs,
+                    basisSize=a.compSize, tailsComputedAtET=ET, maxntails=None)
+            db.insert(datadict=datadict, spec=a.eigenvalues[g]["rentails"][k])
 
 
 
