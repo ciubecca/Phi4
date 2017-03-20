2c293d27cbdd64db4476276da3f61b991834bb42
Lorenzo Vitale <lorenzovital89@gmail.com>
2017-03-20 17:44:01 -0400

    minor mod

--- convert.py
+++ convert.py
@@ -1,12 +1,4 @@
-import sys
-import matplotlib.pyplot as plt
-import scipy
-import math
-from scipy import pi, log, log10, array, sqrt, stats
-from matplotlib import rc
-from cycler import cycler
 import database
-from sys import exit
 
 db = database.Database()
 
