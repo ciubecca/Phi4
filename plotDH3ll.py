e5922645c016577249ceec45e52e3b2e49807bca
Lorenzo Vitale <lorenzovital89@gmail.com>
2017-05-12 15:01:08 -0400

    updated DH3 plots

--- plotDH3ll.py
+++ plotDH3ll.py
@@ -11,30 +11,20 @@ from cycler import cycler
 
 k = +1
 m = 1
-g = 2.
+g = 1.
 eps = {g: -1}
 
 test = False
 
 # (DH3<> nonloc, DH3<> loc, DH3>> loc)
-tlist = ((True,True,False),(True,True,True))
+tlist = ((False,False,False),(True,True,False),(True,True,True))
 
-vectorlist = [[],
-        [(0,2)],
-        [(0,4)],
-        [(0,6)],
-        [(0,8)],
-        [(0,1),(1,4),(-4,1)]
-        ]
 indexList = [(0,0), # V0
-        (0,1), # V2
-        (0,2), # V4 + V0V4
-        (0,3), # V6 + V2V4
-        (0,4), # V4V4
-        (0,5) # V6
+        (0,14) # V6 + V2V4
         ]
 
-
+# Ratio between ELp and ET
+ratioELpET = 2
 # Ratio between ELpp and ELp
 ratioELppELp = 1.5
 frac = ratioELppELp
@@ -45,7 +35,7 @@ plt.rc('axes', prop_cycle=(cycler('color', ['r', 'g', 'b', 'y']) +
     cycler('linestyle', ['-', '--', ':', '-.'])))
 rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
 rc('text', usetex=True)
-output = "png"
+output = "pdf"
 
 argv = sys.argv
 
@@ -64,21 +54,20 @@ ELplist = scipy.linspace(ELpmin, ELpmax, (ELpmax-ELpmin)+1)
 print("ELplist:", ELplist)
 
 
-a = phi4.Phi4(m,L)
+a = phi4.Phi4(m,L,k)
 a.buildBasis(Emax=ET)
-a.computePotential(k)
+a.computePotential()
 a.setglist(glist=[g])
 
+basisl = a.basis
+# print(a.basis.stateList[0:20])
+a.computeLEVs(basisl, loc3=True)
 
-basisl = a.basis[k]
-a.computeLEVs(k, basisl, loc3=True)
-
-a.genHEBasis(k, EL=ELpmax, ELp=ELpmax, ELpp=ratioELppELp*ELpmax)
-print("HE basis size", a.basisH[k].size)
-
+a.genHEBasis(EL=ELpmax, ELp=ELpmax, ELpp=ratioELppELp*ELpmax)
+print("HE basis size", a.basisH.size)
 
 print("Computing HE matrices")
-a.computeHEVs(k)
+a.computeHEVs()
 
 ret = {t:{index:[] for index in indexList} for t in tlist}
 
@@ -91,30 +80,38 @@ for ELp in ELplist:
     for t in tlist:
         nonloc3mix, loc3mix, loc3 = t
 
-        DH3ll = a.computeDH3(k, ET=ET, ELp=ELp, ELpp=ELpp, eps=eps,
+        DH3ll = a.computeDH3(ET=ET, ELp=ELp, ELpp=ELpp, eps=eps,
                 loc3=loc3, loc3mix=loc3mix, nonloc3mix=nonloc3mix)[g].todense()
 
         for index in indexList:
             ret[t][index].append(DH3ll[index])
 
 
-for index in indexList:
+for i,index in enumerate(indexList):
+    print("Index:{}, State:{}".format(index[1], a.basis.stateList[index[1]]))
 
     for t in tlist:
-        plt.plot(ELplist, ret[t][index], label=str(t))
+        if t == (False,False,False):
+            label = "$\Delta H_3^{< <}$"
+        elif t == (True,True,False):
+            label = "$\Delta H_3^{< <} + \Delta H_3^{< >}$"
+        elif t == (True,True,True):
+            label = "$\Delta H_3^{< <} + \Delta H_3^{< >}+\Delta H_3^{> >}$"
+        plt.plot(ELplist, ret[t][index], label=label)
 
-    title = r"$g$={}, $L$={:.1f}, $E_T$={:.1f}, $E_L''={} E_L'$, index={}".\
-            format(g,L,ET,frac,index)
-    fname = "DHplots/DH3_g={}_L={:.1f}_ET={:.1f}_frac={}_index={}.{}".\
-            format(g,L,ET,frac,str(index),output)
+    title = r"$g$={}, $L$={:.1f}, $E_T$={:.1f}, $E_L''={} E_L'$".format(g,L,ET,frac)
+    fname = "DH3_g={}_L={:.1f}_ET={:.1f}_frac={}_index={}.{}".\
+            format(g,L,ET,frac,str(index).replace(" ",""),output)
     loc = "lower right"
+
+    state = ["0","6_0"]
 
     plt.figure(1, figsize=(4., 2.5), dpi=300, facecolor='w', edgecolor='w')
     plt.title(title)
     plt.xlabel(r"$E_{L}'$")
-    plt.ylabel(r"$\Delta H_3 {}$".format(index))
-    plt.legend(loc=loc)
+    plt.ylabel(r"$\langle {} | \Delta H_3 | {}\rangle$".\
+            format(state[0],state[i]))
+    plt.legend(loc=4)
 
     plt.savefig(fname)
-
     plt.clf()
