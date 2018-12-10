b793a1021ea271fb3f2afba13a7e2e2a1fc0dbf9
Lorenzo Vitale <lorenzovitale89@gmail.com>
2018-12-10 17:40:45 -0500

    added test cases

--- testBasis.py
+++ testBasis.py
@@ -2,6 +2,8 @@ import pytest
 import random
 from phi4 import *
 
+tol = 10**-7
+
 # @pytest.mark.skip(reason="Not needed now")
 def test_basis():
     Elist = [10]
@@ -33,4 +35,30 @@ def test_sym():
             a.computePotential()
             assert abs(a.V[4]-a.V[4].transpose()).max() < tol
 
+def test_quartic_spec():
+    Elist = [6]
+    Llist = [5]
+    g2 = [0]
+    g4 = [24]
 
+    vac = [-0.004076480376270055]
+    spece = [[2.035553009079848, 3.199678014214963, 3.356677133229781, 4.082445472190814, 4.21252008745907]]
+    speco = [[0.973434499023974, 3.207424273691506, 4.494048612886267, 4.62886587275236, 5.184264427307492, 5.359318131071841]]
+
+    for Emax,L,g2,g4,e0,se,so in zip(Elist,Llist,g2,g4,vac,spece,speco):
+
+        bases = Basis.fromScratch(m=1, L=L, Emax=Emax)
+        eigs = {}
+
+        for k in (-1,1):
+            a = Phi4(bases[k])
+            a.computePotential()
+
+            a.setg(0, g2, g4/(factorial(4)))
+
+            a.computeEigval(neigs=len(so))
+            eigs[k] = a.eigval
+
+        assert abs(eigs[1][0]-e0) < tol
+        np.testing.assert_array_almost_equal(eigs[1][1:]-eigs[1][0], se)
+        np.testing.assert_array_almost_equal(eigs[-1]-eigs[1][0], so)
