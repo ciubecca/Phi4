98c3996eae1d4fb70c67e7a1cb21dfeafa13ca87
Lorenzo Vitale <lorenzovital89@gmail.com>
2018-12-23 10:20:21 +0100

    improved pert plots

--- ofpt.py
+++ ofpt.py
@@ -8,6 +8,7 @@ from scipy import sparse
 from database import *
 from time import time
 from phi4 import *
+from paramplots import *
 m = 1
 
 if len(argv) < 4:
@@ -32,8 +33,6 @@ ELmin = 5
 ELlist = np.linspace(ELmin, ELmax, 10)
 
 print("L={}, Lambda={}, ELmax={}".format(L, Lambda, ELmax))
-
-
 print("ELlist: ", ELlist)
 
 bases = Basis.fromScratch(m, L, ET, Lambda)
@@ -45,11 +44,15 @@ E0 = {1:0, -1:m}
 
 res = {k:[] for k in (-1,1)}
 
-for k in (-1,1):
+# for k in (-1,1):
+for k in (1,):
+    print("Computing basis...")
     basisH = genHEBasis(bases[k], subidx, ELmax, ELmax)
-    V = genVHl(bases[k], subidx, basisH, L)
-    prop = 1/(E0[k]-array(basisH.energyList))
     print("k={} basis size={}".format(k, basisH.size))
+    print("Computing matrix...")
+    V = genVHl(bases[k], subidx, basisH, L)
+    print("Done")
+    prop = 1/(E0[k]-array(basisH.energyList))
 
     for EL in ELlist:
         idxlist = basisH.subidxlist(EL, Lambda)
@@ -59,8 +62,12 @@ for k in (-1,1):
         deltaE = np.einsum("ij,j,kj", Vsub.todense(), propsub, Vsub.todense())[0][0]
         res[k].append(deltaE)
 
-vac = array(res[1])/L**2-ct0(ELlist)
+vac = array(res[1])/(L**2)-ct0(ELlist)
+# vac = array(res[1])/(L**2)
 plt.plot(ELlist, vac)
-plt.savefig("vacpert.pdf")
+plt.title("L={}".format(L))
+plt.xlabel(r"$E_T$")
+plt.ylabel(r"$\Delta E_0 - c_0(E_T)$")
+plt.savefig("vacpert_L={}.pdf".format(L))
 
 print(vac)
