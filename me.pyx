ff3aed3a1d45166f6d1b8573e7b7719fae22b573
Lorenzo Vitale <lorenzovital89@gmail.com>
2019-04-03 17:40:41 -0400

    imported me.pyx from nlo

--- me.pyx
+++ me.pyx
@@ -1,10 +1,3 @@
-# The function computeME is the most computationally intensive, and it has been ported to cython 
-# Before running the code, this file has to be compiled via:
-# python setup.py build_ext --inplace
-
-
-# cython: linetrace=False
-
 import scipy
 import numpy as np
 from math import factorial, floor, sqrt
@@ -14,10 +7,10 @@ import itertools
 from itertools import combinations, islice, permutations
 from scipy import exp, pi
 import bisect
-from oscillators import *
 cimport cython
 from cpython cimport array as array
 import array
+from operators import *
 
 # XXX Warning usign exponential notation sets this to zero!
 cdef double tol = 0.00000001
@@ -28,54 +21,66 @@ for ncomp1 in (1,2,4,8):
         symFactors[ncomp1][ncomp2] = sqrt(ncomp1/ncomp2)
 
 
-# XXX Review this function, to take Lambda into account?
-def filterDlist(dlist, nd, ntot, helper):
-    """ Return True if the list of momenta can be annihilated by any oscillators, so that 
-    the new state is in the columns basis 
-    dlist: list of potential momenta to annihilate
-    nd: number of annihilation operators
-    ntot: number of creation+annihilation operators
-    helper: Helper object
-    """
-    if nd==ntot:
-        return tuple(sum([np.array(d) for d in dlist])) == (0,0)
-    elif nd==ntot-1:
-        return tuple(sum([np.array(d) for d in dlist])) in helper.allowedWn
-    else:
-        return True
+
+def yieldBasis(basis, idx, EL, helper, nd, nc, dlistPos, oscEnergies, oscList):
+    
+    stateList = basis.stateList
+    torepr2 = helper.torepr2
+    allowedWn = helper.allowedWn
+    energyList = basis.energyList
+
+    state = stateList[idx]
+    e = energyList[idx]
+    
+    cdef char[:,:] osc
+    cdef array.array statevec, newstatevec
+    cdef char *cstatevec
+    cdef char *cnewstatevec
+
+    statevec = array.array('b', torepr2(state))
+    cstatevec = statevec.data.as_chars
+
+    ret = []
+    
+    for dlist in gendlists(state, nd, nd+nc, helper):
+
+        k =dlistPos[dlist]
+
+        imin = bisect.bisect_left(oscEnergies[k], 0-e-tol)
+        imax = bisect.bisect_left(oscEnergies[k], EL-e+tol)
+
+        oscListSub = oscList[k][imin:imax]
+
+        for z in range(len(oscListSub)):
+
+            osc = oscListSub[z]
+
+            newstatevec = array.copy(statevec)
+            cnewstatevec = newstatevec.data.as_chars
+
+            for ii in range(osc.shape[0]):
+                jj = allowedWn[(osc[ii, 0],osc[ii, 1])]
+                Zc = osc[ii, 2]
+                Zd = osc[ii, 3]
+                cnewstatevec[jj] += Zc-Zd
+            
+            ret.append(newstatevec)
+    return ret
 
 
-def gendlists(state, nd, ntot, helper):
-    """ Generates a set of all the sublists of momenta in the state that can be annihilated 
-    state: input state in representation 1
-    nd: number of annihilation operators (number of modes to annihilate)
-    ntot: total number of annihilation and creation operators
-    helper: Helper object
-    """
 
-    x = itertools.chain.from_iterable(([tuple(n)]*Zn for n,Zn in state))
-    dlists = set(tuple(y) for y in combinations(x,nd))
-
-    return (dlist for dlist in dlists if filterDlist(dlist, nd, ntot, helper))
-
-
-# @cython.binding(True)
-# @profile
-def computeME(basis, i, statePos, ignKeyErr, nd, nc, dlistPos, oscFactors, oscList, oscEnergies):
+def computeME(basis, i, destbasis, ignKeyErr, nd, nc, dlistPos, 
+        oscFactors, oscList, oscEnergies):
         """ Compute the matrix elements by applying all the oscillators in the operator
         to an element in the basis
         basis: set of states on which the operator acts
-        i: index of the state in the basis (row index)
-        statePos: dictionary {state: idx} state is in representation 2 (in tuple form)
-            and idx is its position in the column basis 
+        i: index of the state in the basis
+        helper: Helper instance
+        statePos: dictionary where the keys are states in representation 2 (in tuple form)
+            and the values are their position in the basis
         ignKeyErr: this must be set to True if the action of an oscillators on an input state
-            can generate a state not in the columns basis (e.g. must be True for computing Vhh in the NLO formalism)
-        nd: number of annihilation operators 
-        nc: number of creation operators
-        dlistPos: dictionary {dlist: idx} for fast access of various objects related to a given list of momenta to annihilate
-        oscFactors: data structure containing various numerical factors related to the sets of oscillators
-        oscList: data structure with lists of oscillators
-        oscEnergies: data structure containing energy shifts due to action of operators
+            can generate a state not in lookupbasis. This applies only in the computation of Vhh.
+        Otherwise it should be set to False
         """
 
         cdef double x
@@ -88,7 +93,8 @@ def computeME(basis, i, statePos, ignKeyErr, nd, nc, dlistPos, oscFactors, oscLi
         cdef int z, ii, jj
         cdef double[:,:,:] normFactors
 
-        helper = basis.helper
+        helper = destbasis.helper
+        statePos = destbasis.statePos
 
         oscEnergy = helper.oscEnergy
 
@@ -100,13 +106,13 @@ def computeME(basis, i, statePos, ignKeyErr, nd, nc, dlistPos, oscFactors, oscLi
         # I define these local variables outside the loops for performance reasons
         e = basis.energyList[i]
         # Number of components in symmetry representation of state
-        ncomp = basis.ncomp
+        destncomp = destbasis.ncomp
         ncompi = basis.ncomp[i]
         state = basis.stateList[i]
 
         statevec = array.array('b', helper.torepr2(state))
         cstatevec = statevec.data.as_chars
-        
+ 
         allowedWn = helper.allowedWn
         normFactors = helper.normFactors
         Emax = helper.Emax
@@ -118,7 +124,6 @@ def computeME(basis, i, statePos, ignKeyErr, nd, nc, dlistPos, oscFactors, oscLi
                 k = dlistPos[dlist]
             except KeyError as err:
                 raise err
-
 
 # Only select the oscillators such that the sum of the state and oscillator energies
 # lies within the bounds of the lookupbasis energies
@@ -148,7 +153,6 @@ def computeME(basis, i, statePos, ignKeyErr, nd, nc, dlistPos, oscFactors, oscLi
                     cnewstatevec[jj] += Zc-Zd
                     x *= normFactors[Zc, Zd, cstatevec[jj]]
 
-
                 if ignKeyErr:
                     try:
                         j = statePos[bytes(newstatevec)]
@@ -157,9 +161,11 @@ def computeME(basis, i, statePos, ignKeyErr, nd, nc, dlistPos, oscFactors, oscLi
                 else:
                     j = statePos[bytes(newstatevec)]
 
-                x *= symFactors[ncompi][ncomp[j]]
+                x *= symFactors[ncompi][destncomp[j]]
 
                 data.append(x)
                 col.append(j)
 
         return col, data
+
+
