b16247d30abf601f158c2a99456da611b616eeab
Lorenzo Vitale <lorenzovital89@gmail.com>
2019-04-03 17:41:40 -0400

    impoted matrix.py from nlo branch

--- matrix.py
+++ matrix.py
@@ -3,56 +3,70 @@ import scipy
 import scipy.sparse.linalg
 import scipy.sparse
 
+tol = 10**-10
 
-class MatrixConstructor():
-    """ Class used to construct a matrix (or part of it) from a list of oscillators """
+def checkZero(M):
+    try:
+        assert abs(M).max() < tol
+    except AssertionError as e:
+        print(abs(M).max())
+        raise e
+    return
 
-    def __init__(self, basis):
-        """
-        basis: basis for the row and column elements
-        """
-        self.basis = basis
-        self.statePos = basis.statePos
+def checkSymmetric(M):
+    checkZero(M-M.transpose())
+    return
 
-    def buildMatrix(self, Vlist, ignKeyErr=False, sumTranspose=True):
-        """
-        Vlist: list of oscillators
-        ignKeyErr: whether to ignore LookupError when generating a state which is not in the column basis (to be used
-        when the column basis does not contain all the states between Emin and Emax)
-        idxList: list of the row indices to be computed
-        sumTranspose: whether to add to the final result the transpose minus the diagonal
-        """
+def submatrix(V, subidx):
+    """ Return the submatrix for states within smaller cutoffs """
+    return (V.tocsc()[:,subidx]).tocsr()[subidx,]
 
-        basis = self.basis
-        statePos = self.statePos
-        helper = self.basis.helper
+def subrows(V, subidx):
+    return V.tocsr()[subidx,]
 
+def subcolumns(V, subidx):
+    return V.tocsc()[:,subidx]
+
+
+def buildMatrix(basis, Vlist, destbasis=None, idxList=None,
+        ignKeyErr=False, sumTranspose=True):
+    """
+    Vlist: list of oscillators
+    ignKeyErr: whether LookupError when generating a state should be ignored (to be used
+    when the lookupbasis does not contain all the states between Emin and Emax)
+    idxList: list of the row indices to be computed
+    sumTranspose: whether the tranpose of the matrix should be added and the diagonal
+    subtracted
+    """
+
+    if destbasis==None:
+        destbasis = basis
+
+    if idxList==None:
         idxList = range(basis.size)
 
-        # Will construct the sparse matrix in the COO format and then convert it to CSC
-        data = []
-        row = []
-        col = []
+    # Will construct the sparse matrix in the COO format and then convert it to CSC
+    data = []
+    row = []
+    col = []
 
-        # Cicle over list of oscillators
-        for V in Vlist:
-            # Cycle over row indices
-            for i in idxList:
-                colpart, datapart = \
-                    V.computeMatrixElements(basis, i, statePos=statePos, ignKeyErr=ignKeyErr)
-                data += datapart
-                col += colpart
-                row += [i]*len(colpart)
+    for V in Vlist:
+        for i,idx in enumerate(idxList):
+            colpart, datapart = \
+                V.computeMatrixElements(basis, idx, destbasis, ignKeyErr=ignKeyErr)
+            data += datapart
+            col += colpart
+            row += [i]*len(colpart)
 
-        # XXX Does this sum duplicate entries?
-        V = scipy.sparse.coo_matrix((data,(row,col)), shape=(basis.size,basis.size))
+    # XXX Does this sum duplicate entries?
+    # XXX Check
+    V = scipy.sparse.coo_matrix((data,(row,col)),
+            shape=(len(idxList), destbasis.size))
 
-        if sumTranspose:
-            # Add the matrix to its transpose and subtract the diagonal
-            diag_V = scipy.sparse.spdiags(V.diagonal(),0,basis.size,basis.size).tocsc()
-            return (V+V.transpose()-diag_V).tocsc()
-        else:
-            return V.tocsc()
-
-    def __del__(self):
-        del self.statePos
+    if sumTranspose:
+        # Add the matrix to its transpose and subtract the diagonal
+        diag_V = scipy.sparse.spdiags(V.diagonal(),0,basis.size,
+                basis.size).tocsc()
+        return (V+V.transpose()-diag_V).tocsc()
+    else:
+        return V.tocsc()
